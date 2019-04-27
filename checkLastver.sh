23891a88e04517d1c8cf7eb917d9ba2f7355c6c0
amitde69 <amitde7896@gmail.com>
2019-04-27 20:53:40 +0300

    delete image fix

--- checkLastver.sh
+++ checkLastver.sh
@@ -5,7 +5,7 @@ images=$(docker images --format "{{.Tag}}:")
 for tag in $images; do
   if [[ $tag == *"$1"* ]]; then
     echo "its there. deleting version $1..."
-    docekr rmi $2
+    docker rmi $2
   fi
 done
 
